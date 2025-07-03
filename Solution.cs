b7377404adeb2fdb5bd78e2f8731118a622b87f8
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-07-03 09:31:29 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,50 @@
+
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    private record Unit(int index, int toCompare, int toSum) { }
+
+    public long[] FindMaxSum(int[] valuesToCompare, int[] valuesToSum, int maxNumberOfValuesToSum)
+    {
+        Unit[] sortedUnits = createSortedUnits(valuesToCompare, valuesToSum);
+        PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>();
+
+        long currentMaxSum = 0;
+        long[] maxSum = new long[sortedUnits.Length];
+        maxSum[sortedUnits[0].index] = currentMaxSum;
+
+        for (int i = 1; i < sortedUnits.Length; ++i)
+        {
+            minHeap.Enqueue(sortedUnits[i - 1].toSum, sortedUnits[i - 1].toSum);
+            currentMaxSum += sortedUnits[i - 1].toSum;
+
+            if (minHeap.Count > maxNumberOfValuesToSum)
+            {
+                currentMaxSum -= minHeap.Dequeue();
+            }
+
+            if (sortedUnits[i - 1].toCompare < sortedUnits[i].toCompare)
+            {
+                maxSum[sortedUnits[i].index] = currentMaxSum;
+                continue;
+            }
+            maxSum[sortedUnits[i].index] = maxSum[sortedUnits[i - 1].index];
+        }
+
+        return maxSum;
+    }
+
+    private Unit[] createSortedUnits(int[] valuesToCompare, int[] valuesToSum)
+    {
+        Unit[] sortedUnits = new Unit[valuesToCompare.Length];
+        for (int i = 0; i < sortedUnits.Length; ++i)
+        {
+            sortedUnits[i] = new Unit(i, valuesToCompare[i], valuesToSum[i]);
+        }
+        Array.Sort(sortedUnits, (x, y) => x.toCompare - y.toCompare);
+
+        return sortedUnits;
+    }
+}
